4227ad1d2c49f689befcd04b7abdef437ae7630e
Meursault92 <paulormarques92@gmail.com>
2023-05-19 01:59:55 -0300

    Começando os trabalhos

---
+++ script.js
@@ -0,0 +1,11 @@
+script    
+    function logar(){
+        var userName = document.getElementById('userName').value;
+        var password = document.getElementById('password').value;
+       		if(userName == "admin@admin" && password =="admin"){
+                alert('Acesso bem sucedido');
+            	}
+            	else{
+               		alert('Acesso negado');
+            	}
+        }
