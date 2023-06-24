80de008f20422adbdb9dd2064cca4aaf1ad2abab
Meursault92 <paulormarques92@gmail.com>
2023-06-24 09:12:35 -0300

    ajuste de linhas de códigos

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 script    
-    function logar(){
+
+    function log(){
         var userName = document.getElementById('userName').value;
         var password = document.getElementById('password').value;
        		if(userName == "admin@admin" && password =="admin"){
                 alert('Acesso bem sucedido');
-            	}
-            	else{
-               		alert('Acesso negado');
+            	} else{
+               	alert('Acesso negado');
             	}
         }
