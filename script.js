5869bf96bf361e22ffc084f37f1f7e267db7bfc1
Paulo Ricardo <66179718+Meursault92@users.noreply.github.com>
2023-06-30 18:49:23 -0300

    Ajuste script.js
    Ajuste

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 script
 
     function log(){
-    function logar(){
         var userName = document.getElementById('userName').value;
         var password = document.getElementById('password').value;
        		if(userName == "admin@admin" && password =="admin"){
                 alert('Acesso bem sucedido');
             	} else{
                	alert('Acesso negado');
-            	}
-            	else{
-               		alert('Acesso negado');
-            	}
+            }
         }
